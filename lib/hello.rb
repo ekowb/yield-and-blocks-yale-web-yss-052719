395c8f2190c7b77a6ce6107c934b61bf55a60819
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 19:19:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -11,20 +11,6 @@ def hello_t(array)
 end
 end
 
-# def hello_t(array)
-#   if block_given?
-#     i = 0
-#
-#     while i < array.length
-#       yield(array[i])
-#       i = i + 1
-#     end
-#
-#     array
-#   else
-#     puts "Hey! No block was given!"
-#   end
-# end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
